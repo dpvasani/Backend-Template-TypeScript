fff49782b18eabfeeea5ae3f91e83eb8cbf8fefd
dpvasani <vadanidarshan@gmail.com>
2024-12-27 01:01:35 +0530

    DataBase Connection And Server Setup

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
-// Load environment variables from .env file
-dotenv.config(); // This should load your .env variables
 import connectDB from "./db/database";
-// import app from "./app"; // Import the app instance
+import app from "./app";
 
-// Connect to the database and start the server
-const startServer = async () => {
-  try {
-    // Connect to MongoDB
-    await connectDB();
-    console.log("MongoDB Connected Successfully");
+dotenv.config();
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error in Server Setup", error);
+      throw error;
+    });
+
     // Start the server
-    // const PORT = process.env.PORT || 4000;
-    // app.listen(PORT, () => {
-    //   console.log(`Server is running on port ${PORT}`);
-    // });
-  } catch (err) {
-    console.error("MongoDB Connection Failed:", err);
-  }
-};
-
-startServer();
+    app.listen(process.env.PORT || 4000, () => {
+      console.log(`⚙️ Server is running on port ${process.env.PORT || 4000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Failed", err);
+  });
